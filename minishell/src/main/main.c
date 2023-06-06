290934812547015f2f5bbaba660eec37d3526108
Chaleira <pedromchaleira@gmail.com>
2023-06-06 13:27:16 +0100

    okok

--- minishell/src/main/main.c
+++ minishell/src/main/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rteles-f <rteles-f@student.42.fr>          +#+  +:+       +#+        */
+/*   By: plopes-c <plopes-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 13:43:59 by rteles-f          #+#    #+#             */
-/*   Updated: 2023/06/06 13:12:22 by rteles-f         ###   ########.fr       */
+/*   Updated: 2023/06/06 13:27:00 by plopes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,7 @@ void	catch_input(t_control *get)
 void	input_reset(t_control *get)
 {
 	wait(0);
+	// okokokokokokkokokokookokokokokookokkook
 	ft_lstclear(&get->commands, (void *)delete_command);
 	free_shellsplit(get->pieces);
 	if (get->input)
@@ -68,6 +69,7 @@ int	main(int argc, char **argv, char **envp)
 	(void)argv;
 	(void)argc;
 	setup(&get, envp);
+	// a dor e psicologica
 	while (true)
 	{
 		catch_input(&get);
@@ -77,5 +79,6 @@ int	main(int argc, char **argv, char **envp)
 		input_reset(&get);
 		catch_input(&get);
 	}
+	// ja nao me apetece escrever mais comentarios
 	return (1);
 }
