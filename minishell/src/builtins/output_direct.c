27ad24fe3c256f8c7735b769676759810b3d8eae
Rubens Teles Freire Junior <rteles-f@c2r9s3.42lisboa.com>
2023-06-09 18:04:32 +0100

    pwd leak

--- minishell/src/builtins/output_direct.c
+++ minishell/src/builtins/output_direct.c
@@ -6,7 +6,7 @@
 /*   By: rteles-f <rteles-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 17:14:52 by rteles-f          #+#    #+#             */
-/*   Updated: 2023/06/09 17:48:25 by rteles-f         ###   ########.fr       */
+/*   Updated: 2023/06/09 17:57:54 by rteles-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,10 @@
 
 void	output_direct(t_command *command, int index)
 {
-	int	fd;
-
 	if (command->terminal[index + 2])
 	{
 		command->valid = 0;
 		return ;
 	}
-	fd = open(command->terminal[index + 1], O_CREAT | O_RDWR);
+	command->in_pipe[0] = open(command->terminal[index + 1], O_CREAT | O_RDWR);
 }
