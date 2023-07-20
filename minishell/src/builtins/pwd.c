595c073bceac959df449c34a6915ea55f128a66d
home <rubenstfj@LAPTOP-OIKFAVCD>
2023-07-20 07:10:30 +0100

    s

--- minishell/src/builtins/pwd.c
+++ minishell/src/builtins/pwd.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include <minishell.h>
 
 void	pwd_prepare(t_command *command, int index)
 {
