d562cdb16a3d4a0bca0746ca1b7ebfa1598c824e
home <rubenstfj@LAPTOP-OIKFAVCD>
2023-07-25 07:46:05 +0100

    working expantion

--- minishell/src/main/new.c
+++ minishell/src/main/new.c
@@ -12,20 +12,6 @@
 
 #include <minishell.h>
 
-// char	*expand_variable(char *string, int limite)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	HERE;
-// 	while (i < limite)
-// 	{
-// 		if (string[i] == '$')
-// 			;
-// 	}
-// 	return (NULL);
-// }
-
 int	find_pair(char *string, char *jump)
 {
 	int		i;
@@ -91,7 +77,11 @@ char	*fix_quotes_expand(char *string)
 	return (string);
 }
 
-char	**position_and_var(char *string)
+/*
+Mallocs a string with the name of the variable in the input
+returns a list with [0]variable name, [1]string after the variable
+*/
+char	*variable_name(char *string)
 {
 	int		index;
 	char	*envvar;
@@ -99,30 +89,39 @@ char	**position_and_var(char *string)
 	if (!string)
 		return (NULL);
 	index = 0;
-	while (string[index] && !split_case(&string[index]))
+	while (string[index] && string[index] != '\"' && !split_case(&string[index]))
 		index++;
 	envvar = ft_calloc(sizeof(char), index + 1);
 	index = 0;
-	while (string[index] && !split_case(&string[index]))
+	while (string[index] && string[index] != '\"' && !split_case(&string[index]))
 	{
 		envvar[index] = string[index];
 		index++;
 	}
-	return ((char *[]){envvar, &string[index]});
+	return (envvar);
 }
 
-void	insert_envar(char **string, char *varplus, char **envp)
+/*
+Changes '$' to 0 to separate the string before and after the variable.
+Gets variable name and the end of the string.
+Get the found name's expansion.
+Set the expansion to start after '='
+Sets varplus to point only to the end of the string.
+Join the 3 parts.
+*/
+
+void	insert_envar(char **string, char *end, char **envp)
 {
 	char	*build;
 
-	*varplus = 0;
-	varplus = (char *)position_and_var(varplus + 1);
-	envp = get_envaddress(envp, ((char **)varplus)[0]);
+	*end = 0;
+	build = variable_name(end + 1);
+	envp = get_envaddress(envp, build);
 	*envp = ft_strchr(*envp, '=') + 1;
-	free(((char **)varplus)[0]);
-	varplus = ((char **)varplus)[1];
+	end += ft_strlen(build) + 1;
+	free(build);
 	build = ft_strjoin(*string, *envp);
-	ft_stradd(&build, varplus);
+	ft_stradd(&build, end);
 	free(*string);
 	*string = build;
 }
@@ -132,16 +131,20 @@ char	*input_expand(char *input, char **envp)
 	int	i;
 	int	quotes;
 
+	i = 0;
 	while (input[i])
 	{
 		quotes = find_pair(&input[i], "\"\'");
 		i += quotes * (input[i] == '\'');
 		if (quotes && input[i] == '\"')
 		{
-			while (input[i] != '\"')
+			while (input[++i] != '\"')
 			{
 				if (input[i] == '$')
+				{
 					insert_envar(&input, &input[i], envp);
+					return (input);
+				}
 				i++;
 			}
 		}
@@ -157,9 +160,13 @@ expand to another $, it necessary to rerun the function until no
 expantions are executed.
 */
 
-// int	main(int argc, char **argv)
+// int	main(int argc, char **argv, char **envp)
 // {
 // 	(void)argc;
-// 	ft_printf("no\n%s\n", (argv[1]));
-// 	ft_printf("yes\n%s\n", fix_quotes_expand(argv[1]));
+// 	(void)argv;
+// 	char	*input = ft_strdup("asd\"$OLDPWD \"asd");
+// 	ft_printf("no\n%s\n", input);
+// 	char	*get = input_expand(input, envp);
+// 	ft_printf("yes\n%s\n", get);
+// 	free(get);
 // }
